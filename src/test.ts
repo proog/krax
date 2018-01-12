409c0d8e35ca96502db4573ae2c2c68c89191564
Per Mortensen <per@permortensen.com>
2018-01-12 23:53:48 +0100

    Separate tests

--- src/test.ts
+++ src/test.ts
@@ -1,22 +1,25 @@
 import * as assert from "assert";
 import { Krax } from "./index";
 
-async function test() {
-    const krax = new Krax();
-
-    console.log("Running relevance test");
+async function relevance(krax: Krax) {
     let relevanceResults = await krax.searchByRelevance("jens hansen", 5);
     assert.equal(relevanceResults.length, 5, "Relevance results did not have the expected length");
-
-    console.log("Running distance test");
-    let distanceResults = await krax.searchByDistance("jens hansen", 12.34, -12.34, 10);
-    assert.equal(distanceResults.length, 10, "Distance results did not have the expected length");
-
-    console.log("Running 0 result test");
-    let noResults = await krax.search("doesntexist 32r9n289niniosbuisdf");
-    assert.equal(noResults.length, 0);
-
-    console.log("Tests finished");
 }
 
-test();
+async function distance(krax: Krax) {
+    let distanceResults = await krax.searchByDistance("jens hansen", 12.34, -12.34, 10);
+    assert.equal(distanceResults.length, 10, "Distance results did not have the expected length");
+}
+
+async function zeroResults(krax: Krax) {
+    let noResults = await krax.search("doesntexist 32r9n289niniosbuisdf");
+    assert.equal(noResults.length, 0, "Relevance results did not return an empty array");
+}
+
+(async () => {
+    let krax = new Krax();
+    await relevance(krax);
+    await distance(krax);
+    await zeroResults(krax);
+    console.log("Tests finished");
+})();
